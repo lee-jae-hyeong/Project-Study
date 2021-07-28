b30d928d29b219cc86c108413176dff6c51f1c59
lee-jae-hyeong <76590396+lee-jae-hyeong@users.noreply.github.com>
2021-07-28 20:18:52 +0900

    Update train.py

--- Web_app_ratingprediction/Session3_project/p_app/train.py
+++ Web_app_ratingprediction/Session3_project/p_app/train.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from category_encoders import OrdinalEncoder
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 from p_app.models.list import List
 from p_app.models.user import User
 from p_app.naver_scr import movie, code
